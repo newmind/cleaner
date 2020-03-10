46da1d09180b5ea626ae1a360bf1e98920fc3866
kimjg <kimjg@markany.com>
2020-03-10 14:51:44 +0900

    generator -users=100 -duration=30 -interval=1s -size=1024 -path=/tmp 추가

--- generator/generator.go
+++ generator/generator.go
@@ -1,24 +1,115 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"flag"
 	"io"
+	"io/ioutil"
 	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	f, err := os.OpenFile("./test/test", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		log.Fatal(err)
+const appName = "generator"
+
+// Init function, runs before main()
+func init() {
+	// Read command line flags
+	users := flag.Int("users", 1, "Concurrent user count, 동접 유저")
+	duration := flag.Int("duration", 30, "Test duration in seconds")
+	interval := flag.String("interval", "1s", `Interval for file creation("ns", "us" (or "µs"), "ms", "s", "m", "h")`)
+	size := flag.Int64("size", 1024, "Size of file to create in bytes")
+	path := flag.String("path", "", "[Required] Path where to create files")
+
+	flag.Parse()
+
+	parsedInterval, err := time.ParseDuration(*interval)
+	if err != nil || *path == "" {
+		flag.Usage()
+		os.Exit(1)
 	}
-	defer os.Remove(f.Name())
 
-	f.Seek(8, io.SeekCurrent)
-	f.Write([]byte{0x1})
-	f.Close()
+	// Pass the flag values into viper.
+	viper.Set("users", *users)
+	viper.Set("duration", *duration)
+	viper.Set("interval", parsedInterval)
+	viper.Set("size", *size)
+	viper.Set("path", *path)
 
-	fi, err := os.Stat(f.Name())
-	log.Info(fi.Size())
+	formatter := &log.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000",
+		FullTimestamp:   true,
+	}
+	log.SetFormatter(formatter)
+	log.SetLevel(log.DebugLevel)
+}
 
-	log.Infof("%#v, name = %s", f, f.Name())
+func main() {
+	log.Infof("Starting %v...\n", appName)
+
+	os.Mkdir(viper.GetString("path"), os.ModePerm)
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < viper.GetInt("users"); i++ {
+		wg.Add(1)
+		durationChan := time.After(time.Duration(viper.GetInt("duration")) * time.Second)
+		go Worker(i, &wg, durationChan)
+	}
+
+	wg.Wait()
+}
+
+func Worker(index int, wg *sync.WaitGroup, done <-chan time.Time) {
+	defer wg.Done()
+	log.Debugf("Worker[%d] started", index)
+
+	dataDir, err := ioutil.TempDir(viper.GetString("path"),
+		"data"+strconv.Itoa(index)+"-*")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	intervalChan := time.Tick(viper.GetDuration("interval"))
+
+	for {
+		select {
+		case <-intervalChan:
+			f, err := ioutil.TempFile(dataDir, "file-*")
+			if err != nil {
+				log.Error(err)
+				return
+			}
+			//defer os.Remove(f.Name())
+
+			f.Seek(int64(viper.GetInt64("size")-1), io.SeekCurrent)
+			f.Write([]byte{0x1})
+			f.Close()
+
+			//fi, err := os.Stat(f.Name())
+			//log.Info(fi.Size())
+			log.Debugf("[%d] %s", index, f.Name())
+		case <-done:
+			log.Debugf("Worker[%d] done", index)
+			return
+		}
+	}
+}
+
+// Handles Ctrl+C or most other means of "controlled" shutdown gracefully. Invokes the supplied func before exiting.
+func handleSigterm(handleExit func()) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	go func() {
+		<-c
+		handleExit()
+		os.Exit(1)
+	}()
 }
