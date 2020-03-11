4c62c9584edd6ccfaa8707239abb58102faf0ce5
kimjg <kimjg@markany.com>
2020-03-12 00:34:38 +0900

    deletor 성능 테스트

--- cmd/deletor.go
+++ cmd/deletor.go
@@ -3,25 +3,31 @@ package main
 import (
 	"flag"
 	"os"
+	"path/filepath"
 	"runtime/pprof"
 
 	"gitlab.markany.com/argos/cleaner/fileinfo"
 	"gitlab.markany.com/argos/cleaner/scanner"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 const appName = "deletor"
 
 var (
-	path       string
-	lstat      bool
-	cpuprofile string
+	path           string
+	deleteEmptyDir bool
+	lstat          bool
+	dryRun         bool
+	cpuprofile     string
 )
 
 func init() {
 	flag.StringVar(&path, "path", "", "Path to delete")
-	flag.BoolVar(&lstat, "lstat", false, "call lstat() every remove")
+	flag.BoolVar(&deleteEmptyDir, "delete-empty-dir", true, "Delete if dir is empty")
+	flag.BoolVar(&lstat, "lstat", true, "call lstat() every remove")
+	flag.BoolVar(&dryRun, "dry-run", true, "Dry run, doesn't remove files if true")
 	flag.StringVar(&cpuprofile, "cpuprofile", "", "CPU profile")
 	flag.Parse()
 
@@ -29,6 +35,8 @@ func init() {
 		flag.Usage()
 		os.Exit(1)
 	}
+
+	viper.Set("dryRun", dryRun)
 
 	formatter := &log.TextFormatter{
 		TimestampFormat: "2006-01-02T15:04:05.000",
@@ -55,12 +63,36 @@ func main() {
 	scannedFiles = scanner.ScanAllFiles([]string{path})
 	log.Infof("  scanned %v files\n", len(scannedFiles))
 
-	for _, fi := range scannedFiles {
-		if lstat {
-			_, _ = os.Lstat(fi.Path)
+	var deletedSize int64
+
+	for len(scannedFiles) > 0 {
+		last := scannedFiles[len(scannedFiles)-1]
+		if fi, err := os.Lstat(last.Path); err == nil {
+			deletedSize += fi.Size()
+			if err := remove(last.Path); err != nil {
+				log.Error(err)
+			}
+			log.Debug("Deleted :", last.Path)
+			if deleteEmptyDir {
+				// try removing dir if empty
+				err := remove(filepath.Dir(last.Path))
+				if err == nil {
+					log.Debug("Deleted[dir] :", filepath.Dir(last.Path))
+				}
+			}
 		}
-		os.Remove(fi.Path)
+		// remove from slice
+		scannedFiles = scannedFiles[:len(scannedFiles)-1]
 	}
+	log.Infof("Total size deleted %d", deletedSize)
 
 	log.Info("Exit")
+}
+
+func remove(path string) error {
+	dryRun := viper.GetBool("dry-run")
+	if dryRun {
+		return nil
+	}
+	return os.Remove(path)
 }
