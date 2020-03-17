28d061f97b5a030dda1bb3e162c1839695d5630a
kimjg <kimjg@markany.com>
2020-03-17 17:23:44 +0900

    맥에서 rlimit

---
+++ init_darwin.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+)
+
+func init() {
+	// increase file limit
+	// https://stackoverflow.com/questions/17817204/how-to-set-ulimit-n-from-a-golang-program
+	var rLimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		fmt.Println("Error Getting rlimit ", err)
+		return //os.Exit(1)
+	}
+	fmt.Println("Current rLimit:", rLimit)
+
+	//rLimit.Max = 20480
+	rLimit.Cur = 20480
+	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		fmt.Println("Error Setting Rlimit ", err)
+	}
+}
--- init_linux.go
+++ init_linux.go
@@ -12,9 +12,16 @@ func init() {
 	if runtime.GOOS == "linux" {
 		// https://stackoverflow.com/questions/17817204/how-to-set-ulimit-n-from-a-golang-program
 		var rLimit syscall.Rlimit
+		err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+		if err != nil {
+			fmt.Println("Error Getting rlimit ", err)
+			return //os.Exit(1)
+		}
+		fmt.Println("Current rLimit:", rLimit)
+
 		rLimit.Max = 900000
 		rLimit.Cur = 900000
-		err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+		err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 		if err != nil {
 			fmt.Println("Error Setting Rlimit ", err)
 		}
